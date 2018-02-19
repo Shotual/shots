18881c12bb25d98c49a2057c418b322c80e3ae5b
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2018-02-19 10:11:06 +0100

    creacion de carpeta sqllite y creacion de nuestro objeto contact ahora te intruduzo la carpeta con el script de la base de datos y el php

---
+++ appexamen/src/main/java/sqllite/Contact.java
@@ -0,0 +1,100 @@
+package sqllite;
+
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by guille on 19/2/18.
+ */
+
+
+
+public class Contact {
+
+    //private variables
+    int _id;
+    String _name;
+    double lat,lon;
+   // Marker perfilMarker;
+
+    // Empty constructor
+    public Contact(){
+
+    }
+    public Contact(int id, String name, double lat, double lon){
+        this._id = id;
+        this._name = name;
+        this.lat=lat;
+        this.lon=lon;
+    }
+
+
+    // constructor
+    public Contact(String name, double lat, double lon){
+        this._name = name;
+        this.lat=lat;
+        this.lon=lon;
+
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("Nombre", _name);
+        result.put("lat", lat);
+        result.put("lon", lon);
+
+
+        return result;
+    }
+
+
+  //  public void setMarkerEnsalada(Marker markerEnsalada){
+//        this.perfilMarker = markerEnsalada;
+  //  }
+    //public Marker getMarkerEnsalada(){
+      //  return perfilMarker;
+    //}
+
+    // getting ID
+    public int getID(){
+        return this._id;
+    }
+
+    // setting id
+    public void setID(int id){
+        this._id = id;
+    }
+
+    // getting name
+    public String getName(){
+        return this._name;
+    }
+
+    // setting name
+    public void setName(String name){
+        this._name = name;
+    }
+
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+
+}
