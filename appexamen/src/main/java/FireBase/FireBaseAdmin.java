09566667c6490dc0386cf3f0cb189a2a5139363d
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2018-02-19 09:38:00 +0100

    clases dataholder firebaseadmin y firebaselistener implementadas

--- appexamen/src/main/java/FireBase/FireBaseAdmin.java
+++ appexamen/src/main/java/FireBase/FireBaseAdmin.java
@@ -4,5 +4,117 @@ package FireBase;
  * Created by guille on 19/2/18.
  */
 
+import android.app.Activity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by guille on 19/12/17.
+ */
 public class FireBaseAdmin {
+
+    public FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRefRaiz;
+
+    public FireBaseAdminListener listener;
+    public FirebaseUser user;
+
+    public FireBaseAdmin(){
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRefRaiz=database.getReference();
+    }
+
+    public void LogOut(){
+        FirebaseAuth.getInstance().signOut();
+    }
+
+
+    public void setListener(FireBaseAdminListener listener){
+
+        this.listener=listener;
+    }
+
+    public void registerEmailPass(String email, String password, Activity activity){
+
+
+
+    }
+    public void loginEmailPass(String email, String password, Activity activity) {
+
+
+    }
+    public void descargarYObservarRama(final String rama){
+
+        DatabaseReference refBranch = myRefRaiz.child(rama);
+        refBranch.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                listener.firebaseAdmin_ramaDescargada(rama,dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                listener.firebaseAdmin_ramaDescargada(rama,null);
+            }
+        });
+    }
+
+    public void insertarenrama (String rutaRama, Map<String,Object> valores){
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(rutaRama ,valores);
+        myRefRaiz.updateChildren(childUpdates);
+    }
 }
+
--- appexamen/src/main/java/FireBase/FireBaseAdminListener.java
+++ appexamen/src/main/java/FireBase/FireBaseAdminListener.java
@@ -4,5 +4,12 @@ package FireBase;
  * Created by guille on 19/2/18.
  */
 
+
+import com.google.firebase.database.DataSnapshot;
+
 public interface FireBaseAdminListener {
+
+    public  void firebaseAdmin_registerOk(Boolean blOk);
+    public void firebaseAdmin_loginOk(Boolean blOk);
+    public void firebaseAdmin_ramaDescargada(String rama, DataSnapshot dataSnapshot);
 }
