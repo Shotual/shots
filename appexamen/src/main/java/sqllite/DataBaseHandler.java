befaa878c521584c349d89eb54c46a5906c4db98
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2018-02-19 10:26:21 +0100

    creacion de datahandler donde se creara la base de datos interna donde almacenaremos mas tarde la informacion recibida en forma de json y crearemos la misma tabla que tenemos en sql en sqllite con su respetios datos como nombre id lat y lon

---
+++ appexamen/src/main/java/sqllite/DataBaseHandler.java
@@ -0,0 +1,145 @@
+package sqllite;
+
+/**
+ * Created by guille on 19/2/18.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by guille on 17/2/18.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseHandler extends SQLiteOpenHelper {
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 7;
+
+    // Database Name
+    private static final String DATABASE_NAME = "contactsManager";
+
+    // Contacts table name
+    private static final String TABLE_CONTACTS = "contacts";
+    private static final String TABLE_USERS = "usuarios";
+
+    // Contacts Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_LAT = "lat";
+    private static final String KEY_LON = "lon";
+
+    public DataBaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.v("YONYBBDD","SUPER---->>");
+        getWritableDatabase();
+
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                +KEY_LAT + " DOUBLE,"+ KEY_LON + " DOUBLE"+")";
+        Log.v("YONYBBDD","DATABASE---->>"+CREATE_CONTACTS_TABLE);
+        db.execSQL(CREATE_CONTACTS_TABLE);
+
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+
+        // Create tables again
+        onCreate(db);
+    }
+    // Adding new contact
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, contact.getID());
+        values.put(KEY_NAME, contact.getName()); // Contact Name
+        values.put(KEY_LAT, contact.getLat());
+        values.put(KEY_LON, contact.getLon());// Contact Phone Number
+
+        // Inserting Row
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close(); // Closing database connection
+    }
+
+
+    // Getting single contact
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID,
+                        KEY_NAME, KEY_LAT, KEY_LON}, KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getDouble(2), cursor.getDouble(3));
+        // return contact
+        return contact;
+    }
+
+
+    // Getting All Contacts
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<Contact>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_CONTACTS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Contact contact = new Contact();
+                contact.setID(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setLat(cursor.getInt(2));
+                contact.setLon(cursor.getInt(3));
+
+                // Adding contact to list
+                contactList.add(contact);
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return contactList;
+    }
+}
