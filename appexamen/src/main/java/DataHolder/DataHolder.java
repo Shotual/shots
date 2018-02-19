06a78f2740be256d0f5f7a7967523a72bb86eaa1
Guillermo Tuvilla Gómez <guillermo.tuvilla@live.u-tad.com>
2018-02-19 09:35:55 +0100

    carpetas DataHolder y Firebase creadas en proceso de meter las clases para cada carpeta en caso de firebase crearemos una clase firebaseadmin y un listener de esa clase

---
+++ appexamen/src/main/java/DataHolder/DataHolder.java
@@ -0,0 +1,8 @@
+package DataHolder;
+
+/**
+ * Created by guille on 19/2/18.
+ */
+
+public class DataHolder {
+}
---
+++ appexamen/src/main/java/FireBase/FireBaseAdminListener.java
@@ -0,0 +1,8 @@
+package FireBase;
+
+/**
+ * Created by guille on 19/2/18.
+ */
+
+public interface FireBaseAdminListener {
+}
